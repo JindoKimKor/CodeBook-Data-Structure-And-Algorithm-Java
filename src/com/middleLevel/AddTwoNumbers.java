a09864257b522bd2fe16a81916afda0bf20d8ea8
Jindo Kim <jindokim.kor@gmail.com>
2024-01-25 16:21:15 -0500

    Prepare test setup for linked list based number addition

--- src/com/middleLevel/AddTwoNumbers.java
+++ src/com/middleLevel/AddTwoNumbers.java
@@ -16,33 +16,27 @@ public class AddTwoNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] firstNumberArray = {1,2,3,4,5,6,7};
+		int[] firstNumberArray = {9,9,9,9,9,9,9};
 		int[] secondNumberArray = {9,9,9,9};
 		
-		if (firstNumberArray.length > 0) {
-			ListNode firstNumbersArrayFirstReferencing = new ListNode(firstNumberArray[0]);
-			ListNode current = firstNumbersArrayFirstReferencing;
-			for (int i = 1; i < firstNumberArray.length; i++) {
-				current.next = new ListNode(firstNumberArray[i]);
-				//only referencing/pointer update, not class's field values
-				current = current.next;
-			}
-			
-			current = firstNumbersArrayFirstReferencing;
-			while (current != null) {
-				System.out.println(current.val);
-				current = current.next;
-			}
-		}
+		ListNode firstNumbersArrayFirstPointer = arrayToListNode(firstNumberArray);
+		ListNode secondNumbersArrayFirstPointer = arrayToListNode(secondNumberArray);
 		
-		if (secondNumberArray.length > 0) {
-			
-		}
-
 	}
-//	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//		   
-//    }
+	private static ListNode arrayToListNode(int[] array) {
+		if(array.length == 0) { return null; }
+		ListNode head = new ListNode(array[0]);
+		ListNode current = head;
+		for (int i = 0; i < array.length; i++) {
+			current.next = new ListNode(array[i]);
+			//only referencing/pointer update(switch), not class's field values
+			current = current.next;
+		}
+		return head;
+	}
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		   
+    }
 
 
 }
